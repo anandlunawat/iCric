53a78cb98590bfddf88aad48f99e05d22d2eb14f
Anand Lunawat <aplunawat@gmail.com>
2023-07-06 22:36:12 +0530

    Cards Added

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,6 +1,6 @@
 export default function Cards() {
     return (
-        <div className="flex flex-col w-[50%] gap-3 rounded-2xl border-[1px] shadow-lg p-4 m-4">
+        <div className="flex flex-col w-[30%] gap-3 rounded-2xl border-[1px] shadow-lg p-4 m-4">
             <span className="text-xl font-medium">Result</span>
             <span className="font-medium text-gray-500">1st ODI (D/N)</span>
             <div className="flex flex-row gap-4">
