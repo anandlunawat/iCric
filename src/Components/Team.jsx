22c38d6185cd366aa2b4d0c783957c63b4803ea9
Anand Lunawat <aplunawat@gmail.com>
2023-08-05 00:56:15 +0530

    Updated teamCards

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -4,7 +4,7 @@ export default function Team() {
     return (
         <div className="flex flex-col p-6">
             <span className="self-center text-3xl font-extrabold text-indigo-900">Our team</span>
-            <div className="flex flex-row flex-wrap gap-20 mt-4 md:gap-32 max-sm:gap-10 md:w-full">
+            <div className="flex flex-row flex-wrap items-center justify-center gap-20 mt-6 md:gap-32 max-sm:gap-10 md:w-full">
                 <TeamCArds
                     url={"/stadium.jpg"}
                     name={"Full Name"}
