1341eb002b26f07c5b0b6628c60254e5b69ec16c
Anand Lunawat <aplunawat@gmail.com>
2023-08-04 12:22:08 +0530

    Added Home and About Us page

--- src/Components/TeamCards.jsx
+++ src/Components/TeamCards.jsx
@@ -2,7 +2,7 @@ import {AiOutlineInstagram,AiFillLinkedin,AiFillFacebook,AiFillTwitterCircle} fr
 
 export default function TeamCArds(props) {
     return (
-        <div className="flex flex-col items-center justify-center gap-2">            
+        <div className="flex flex-col items-center justify-center gap-2 max-sm:w-full">            
             <img src={props.url} className="w-40 h-40 rounded-full max-md:h-20 max-md:w-20"/>            
             <span className="text-lg font-semibold text-lime-500">{props.name}</span>
             <div className="flex gap-2 text-indigo-900">
