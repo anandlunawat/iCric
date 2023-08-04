d78fa6a2f6b539612edfcb1812b2923b3e80a76c
Anand Lunawat <aplunawat@gmail.com>
2023-08-04 12:25:37 +0530

    Added Home and About Us page

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -4,7 +4,7 @@ export default function Team() {
     return (
         <div className="flex flex-col p-6">
             <span className="self-center text-3xl font-extrabold text-indigo-900">Our team</span>
-            <div className="flex flex-row flex-wrap gap-32 mt-4 max-sm:gap-5">
+            <div className="flex flex-row flex-wrap gap-32 mt-4 max-sm:gap-10">
                 <TeamCArds
                     url={"/stadium.jpg"}
                     name={"Full Name"}
