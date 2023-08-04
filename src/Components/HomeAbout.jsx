539ddfea0f192bfe8b9b57cfe9e9d899116561b4
PRATHAM MORE <pratham02more20@gmail.com>
2023-08-05 02:16:56 +0530

    gap

--- src/Components/HomeAbout.jsx
+++ src/Components/HomeAbout.jsx
@@ -2,7 +2,7 @@ export default function HomeAbout() {
     return (
         <div className="flex flex-col p-6 bg-gray-100">
             <span className="self-center text-3xl font-extrabold text-indigo-900">About Us</span>
-            <div className="flex flex-col mb-2 lg:flex lg:flex-row">
+            <div className="flex flex-col lg:gap-36 mb-2 lg:flex lg:flex-row">
                 <div className="flex flex-col gap-4 p-4 basis-1/2">
                     <span className="text-3xl font-extrabold text-indigo-900 max-lg:text-center">WE LOVE CRICKET</span>
                     <span className="font-normal text-indigo-900">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</span>
--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,25 +1,28 @@
 import Navbar from "./Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import routes from "../route.js"
+import routes from "../route.js";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
-    return (
-        <div>
-            <BrowserRouter>
-                <Navbar />
-                <Routes>
-                    {
-                        routes.map((route) => {
-                            return (
-                                <Route key={route.name} path={route.path} element={route.element} />
-                            )
-                        })
-                    }
-                </Routes>
-                <div>{children}</div>
-                <Footer />
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.name}
+                path={route.path}
+                element={route.element}
+              />
+            );
+          })}
+        </Routes>
+        <div>{children}</div>
+        <Footer />
+        <Navbar />
+      </BrowserRouter>
+    </div>
+  );
 }
